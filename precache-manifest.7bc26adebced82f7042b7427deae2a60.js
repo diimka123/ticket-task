9c48a23470c259fd92830ea95ce0ac57c01f06d9
Dmitry <riiba228@gmail.com>
2020-11-22 20:56:43 +0300

    deploy: deploy commit

---
+++ precache-manifest.7bc26adebced82f7042b7427deae2a60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1adced5d97a506bb02802f76c8a4eed",
+    "url": "/ticket-task/index.html"
+  },
+  {
+    "revision": "9843917f5c945e13ef76",
+    "url": "/ticket-task/static/css/main.fb90be2b.chunk.css"
+  },
+  {
+    "revision": "e0c4c533b428a70af9d0",
+    "url": "/ticket-task/static/js/2.f0d2ce75.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ticket-task/static/js/2.f0d2ce75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9843917f5c945e13ef76",
+    "url": "/ticket-task/static/js/main.88eaad90.chunk.js"
+  },
+  {
+    "revision": "1753e9b9e2bf5ddb902c",
+    "url": "/ticket-task/static/js/runtime-main.6fbe3b5e.js"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/ticket-task/static/media/avia-logo.0c089234.svg"
+  }
+]);
